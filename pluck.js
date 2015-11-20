ace8aee7de2b0ff3da5642d734e9bd6369326dee
= <do_web@163.com>
2015-11-20 12:23:13 +0800

    .\pick.js

--- pluck.js
+++ pluck.js
@@ -1,4 +1,5 @@
 //需要取user_id的数组
+//pluck
 var arr =[
     {"id":1749,"tik_id":0,"creator_id":0,"group_id":26,"user_id":65895,"type_id":0,"group_seq":0,"begin_time":"1999-12-31T16:00:00.000Z","end_time":"2999-12-31T16:00:00.000Z","kv":null,"status":0,"created_at":null,"updated_at":null},
     {"id":1909,"tik_id":0,"creator_id":0,"group_id":26,"user_id":66349,"type_id":0,"group_seq":0,"begin_time":"1999-12-31T16:00:00.000Z","end_time":"2999-12-31T16:00:00.000Z","kv":null,"status":0,"created_at":null,"updated_at":null},
@@ -10,5 +11,6 @@ var arr =[
     {"id":2011,"tik_id":0,"creator_id":0,"group_id":26,"user_id":5501,"type_id":0,"group_seq":0,"begin_time":"1999-12-31T16:00:00.000Z","end_time":"2999-12-31T16:00:00.000Z","kv":null,"status":0,"created_at":null,"updated_at":null},
     {"id":2013,"tik_id":0,"creator_id":0,"group_id":26,"user_id":21201,"type_id":0,"group_seq":0,"begin_time":"1999-12-31T16:00:00.000Z","end_time":"2999-12-31T16:00:00.000Z","kv":null,"status":0,"created_at":null,"updated_at":null}];
 
+console.log(_.pluck(arr, ['user_id','tik_id']));
 console.log(_.pluck(arr, 'user_id'));
 //[ 65895, 66349, 66160, 24201, 701, 24101, 23901, 5501, 21201 ]
