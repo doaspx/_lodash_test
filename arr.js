4089af25eba66aff0a1b73b332e0286dad4f58ea
= <do_web@163.com>
2015-12-24 10:21:00 +0800

    add

--- arr.js
+++ arr.js
@@ -1,7 +1,23 @@
 /**
  * Created by zhanghongtao on 2015/11/16.
  */
+var _ = require('lodash');
 console.log('begin');
+//chunk##################说明
+// 1、chunk
+// n. 大块；矮胖的人或物
+// 用法： _.chunk(array,number)  根据number对array进行均等的分块，如果array不能被number平分，则会留下一个余下的块。
+// 当 size<=1的时候，都是按1等分
+//2、compact
+// adj. 紧凑的，紧密的；简洁的
+// vt. 使简洁；使紧密结合
+// Creates an array with all falsey values removed. The values false, null, 0, "", undefined, and NaN are falsey.
+//3、difference 从数组中过滤元素
+//用法：_.difference(array,[values])
+//参数说明： array:要被检查/过滤的数组。
+//values:要被在array中剔除的值的集合
+
+
 
 // var q =  _.last([1,{},'c']);
 //var q = _.initial([1,{},'c'])
